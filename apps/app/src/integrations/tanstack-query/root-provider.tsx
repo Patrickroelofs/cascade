9ac60a5dd47c7218845536d47ed68331bdae9301
Patrick Roelofs <contact@patrickroelofs.com>
2026-07-21 10:03:09 +0200

    chore: remove TODO comment for error logging
    Removed TODO comment about logging errors in platform.

--- apps/app/src/integrations/tanstack-query/root-provider.tsx
+++ apps/app/src/integrations/tanstack-query/root-provider.tsx
@@ -4,7 +4,6 @@ export function getContext() {
 	const queryClient = new QueryClient({
 		mutationCache: new MutationCache({
 			onError: (error) => {
-				// TODO: Log error in platform for investigation
 				console.error(error);
 			},
 		}),
